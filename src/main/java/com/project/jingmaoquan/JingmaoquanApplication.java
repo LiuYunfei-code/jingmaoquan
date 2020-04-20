a0ed0a9532aee4fb0074d94ef2c3c6f656714529
Liu <1311577860@qq.com>
2020-04-20 19:44:32 +0800

    init repo

---
+++ src/main/java/com/project/jingmaoquan/JingmaoquanApplication.java
@@ -0,0 +1,13 @@
+package com.project.jingmaoquan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingmaoquanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JingmaoquanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/jingmaoquan/JingmaoquanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.jingmaoquan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingmaoquanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
